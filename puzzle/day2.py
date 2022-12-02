99d1fb2fb85bb76183fe680c32e973f00e472844
Sepehr Tayari <sepehrtayari@gmail.com>
2022-12-02 11:11:20 +0100

    Update day2.py

--- puzzle/day2.py
+++ puzzle/day2.py
@@ -1,8 +1,4 @@
 
-test = """A Y
-B X
-C Z
-"""
 def main():
     input = open('../input/input2.txt', 'r').read()
     input_list = input.rstrip().split('\n')
@@ -16,9 +12,7 @@ def main():
   # B 1  5  9 
   # C 7  2  6
 
-
-
-    points = [[4, 8, 3 ],
+    points =   [[4, 8, 3 ],
                 [1, 5, 9 ],
                 [7, 2, 6 ]]
     
@@ -37,20 +31,6 @@ def translate(c):
     if ( c == 'B' ) or ( c == 'Y' ): return 1
     if ( c == 'C' ) or ( c == 'Z' ): return 2
     return
-def play(opponent, outcome):
-    # X Lose
-    if outcome == 'X':
-        if(opponent == 'A'): return 'B'
-        if(opponent == 'B'): return 'C'
-        if(opponent == 'C'): return 'A'
-    # Y Draw
-    if outcome == 'Y':
-        return opponent
-    # Z Win
-    if outcome == 'Z':
-        if(opponent == 'A'): return 'C'
-        if(opponent == 'B'): return 'A'
-        if(opponent == 'C'): return 'B'
 
 if __name__ == '__main__':
     main()
